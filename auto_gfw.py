a75d57dabe82c33b4d133b41079d521aeb1d815f
chengshuai <chengshuai232052@gmail.com>
2021-02-28 16:20:06 +0800

    add vmess file

--- auto_gfw.py
+++ auto_gfw.py
@@ -3,12 +3,18 @@ import ssl
 import re
 
 
-ssl._create_default_https_context = ssl._create_unverified_context
+def generate_vmess():
+    ssl._create_default_https_context = ssl._create_unverified_context
+    url = 'https://my.ishadowx.biz'
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'}
+    req = urllib.request.Request(url, headers=headers)
+    html = urllib.request.urlopen(req).read().decode('utf-8')
+    pattern = re.compile(r'vmess:.*?\n')
+    res = pattern.findall(html)
+    #return res[2]
+    return ''.join(res)
 
-url = 'https://my.ishadowx.biz'
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'}
-req = urllib.request.Request(url, headers=headers)
-html = urllib.request.urlopen(req).read().decode('utf-8')
-pattern = re.compile(r'vmess:.*?\n')
-res = pattern.findall(html)
-print(res[0])
+
+if __name__ == '__main__':
+    output = generate_vmess()
+    print(output)
--- sendemail.py
+++ sendemail.py
@@ -3,6 +3,7 @@ from email import encoders
 from email.header import Header
 from email.mime.text import MIMEText
 from email.utils import parseaddr, formataddr
+from auto_gfw import generate_vmess
 
 
 def _format_addr(s):
@@ -15,14 +16,21 @@ password = '05545714184'
 smtp_server = 'smtp.163.com'
 to_addr = '1486361198@qq.com'
 
-msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
-msg['From'] = _format_addr('Python爱好者 <%s>' % from_addr)
-msg['To'] = _format_addr('管理员 <%s>' % to_addr)
-msg['Subject'] = Header('来自SMTP的问候……', 'utf-8').encode()
+#mail_context = '''
+#<html><body><h1>{0}</h1>
+#<p>send by <a href="http://www.python.org">Python</a></p>
+#</body></html>
+#'''.format(generate_vmess())
+
+
+msg = MIMEText(generate_vmess(), 'plain', 'utf-8')
+msg['From'] = _format_addr('Raspberry Robot <%s>' % from_addr)
+msg['To'] = _format_addr('1486361198 <%s>' % to_addr)
+msg['Subject'] = Header('iShadow', 'utf-8').encode()
 
 
 server = smtplib.SMTP(smtp_server, 25)
-server.set_debuglevel(1)
+#server.set_debuglevel(1)
 server.login(from_addr, password)
 server.sendmail(from_addr, [to_addr], msg.as_string())
 server.quit()
